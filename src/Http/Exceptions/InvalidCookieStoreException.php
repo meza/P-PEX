5c73424cd13e4c99467021ede8fad85ed154f2bd
meza <reg@meza.hu>
2010-03-13 13:08:06 +0100

    Invalid Cookie Store Exception added
    Signed-off-by: meza <reg@meza.hu>

---
+++ src/Http/Exceptions/InvalidCookieStoreException.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * InvalidCookieStoreException.php
+ *
+ * Holds the InvalidCookieStoreException class
+ *
+ * PHP Version: 5
+ *
+ * @category File
+ * @package  Http
+ * @author   meza <meza@meza.hu>
+ * @license  GPL3.0
+ *                    GNU GENERAL PUBLIC LICENSE
+ *                       Version 3, 29 June 2007
+ *
+ * Copyright (C) 2007 Free Software Foundation, Inc. <http://fsf.org/>
+ * Everyone is permitted to copy and distribute verbatim copies
+ * of this license document, but changing it is not allowed.
+ * *
+ * @version  GIT: $Id$
+ * @link     http://www.assembla.com/spaces/p-pex
+ */
+
+/**
+ * The InvalidCookieStoreException fires, when an invalid cookie store was found
+ *
+ * PHP Version: 5
+ *
+ * @category Class
+ * @package  Http
+ * @author   meza <meza@meza.hu>
+ * @license  GPLv3 <http://www.gnu.org/licenses/>
+ * @link     http://www.assembla.com/spaces/p-pex
+ */
+class InvalidCookieStoreException extends Exception
+{
+    protected $message = "The given cookie store is not writable";
+}//end class
+
+?>
