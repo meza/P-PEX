6de1716c041ef709af530f308f85cb57a58639ef
meza <reg@meza.hu>
2010-03-28 13:19:33 +0200

    Added exchange response interface
    Signed-off-by: meza <reg@meza.hu>

---
+++ src/ExchangeStore/ExchangeResponse.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * ExchangeResponse.php
+ *
+ * Holds the ExchangeResponse interface
+ *
+ * PHP Version: 5
+ *
+ * @category File
+ * @package  Response
+ * @author   meza <meza@meza.hu>
+ * @license  GPL3.0
+ *                    GNU GENERAL PUBLIC LICENSE
+ *                       Version 3, 29 June 2007
+ *
+ * Copyright (C) 2007 Free Software Foundation, Inc. <http://fsf.org/>
+ * Everyone is permitted to copy and distribute verbatim copies
+ * of this license document, but changing it is not allowed.
+ * *
+ * @version  GIT: $Id$
+ * @link     http://www.assembla.com/spaces/p-pex
+ */
+
+/**
+ * The ExchangeResponse interface is used to identify all responses from the server
+ *
+ * PHP Version: 5
+ *
+ * @category Interface
+ * @package  Response
+ * @author   meza <meza@meza.hu>
+ * @license  GPLv3 <http://www.gnu.org/licenses/>
+ * @link     http://www.assembla.com/spaces/p-pex
+ */
+interface ExchangeResponse
+{
+
+}//end interface
+
+?>
