fef3f21dcb0c70a7dc50d71a245d8bfe6b6ae0d0
meza <reg@meza.hu>
2010-05-23 16:27:13 +0200

    Added interface. Closes #12

---
+++ src/Pex/CalendarHandler.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * CalendarHandler.php
+ *
+ * Holds the CalendarHandler interface
+ *
+ * PHP Version: 5
+ *
+ * @category File
+ * @package  Pex
+ * @author   meza <meza@meza.hu>
+ * @license  GPL3.0
+ *                    GNU GENERAL PUBLIC LICENSE
+ *                       Version 3, 29 June 2007
+ *
+ * Copyright (C) 2007 Free Software Foundation, Inc. <http://fsf.org/>
+ * Everyone is permitted to copy and distribute verbatim copies
+ * of this license document, but changing it is not allowed.
+ * * 
+ * @link     http://www.assembla.com/spaces/p-pex
+ */
+
+/**
+ * The CalendarHandler interface declares the api for calendar handling
+ *
+ * PHP Version: 5
+ *
+ * @category Interface
+ * @package  Pex
+ * @author   meza <meza@meza.hu>
+ * @license  GPLv3 <http://www.gnu.org/licenses/>
+ * @link     http://www.assembla.com/spaces/p-pex
+ */
+interface CalendarHandler
+{
+
+
+    /**
+     * Creates an event in the store
+     *
+     * @param CalendarEvent $event The event to be stored
+     *
+     * @return string url of the newly created event
+     */
+    public function createEvent(CalendarEvent $event);
+
+
+    /**
+     * Removes an entry from a given url
+     *
+     * @param string $url endpoint
+     *
+     * @return bool True on success, false otherwise
+     */
+    public function deleteEvent($url);
+
+
+    /**
+     * Lists al events in the exchange store
+     *
+     * @return CalendarEvent[]
+     */
+    public function listEvents();
+
+
+}//end interface
+
+?>
