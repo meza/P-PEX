6abfaa837254c0defbeb70af16160610f71fd495
meza <reg@meza.hu>
2010-05-29 15:38:08 +0200

    Added Calendar event creator param object #13
    Signed-off-by: meza <reg@meza.hu>

---
+++ src/ExchangeStore/HttpParams/CalendarEventCreateHttpParam.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * CalendarEventCreateHttpParam.php
+ *
+ * Holds the CalendarEventCreateHttpParam class
+ *
+ * PHP Version: 5
+ *
+ * @category File
+ * @package  ExchangeStore
+ * @author   meza <meza@meza.hu>
+ * @license  GPL3.0
+ *                    GNU GENERAL PUBLIC LICENSE
+ *                       Version 3, 29 June 2007
+ *
+ * Copyright (C) 2007 Free Software Foundation, Inc. <http://fsf.org/>
+ * Everyone is permitted to copy and distribute verbatim copies
+ * of this license document, but changing it is not allowed.
+ * * 
+ * @link     Expression project.url is undefined on line 14, column 16 in Templates/Scripting/PHPClass.
+ */
+
+/**
+ * The CalendarEventCreateHttpParam class is responsible for creating Calendar
+ * items
+ *
+ * PHP Version: 5
+ *
+ * @category Class
+ * @package  ExchangeStore
+ * @author   meza <meza@meza.hu>
+ * @license  GPLv3 <http://www.gnu.org/licenses/>
+ * @link     Expression project.url is undefined on line 26, column 16 in Templates/Scripting/PHPClass.
+ */
+class CalendarEventCreateHttpParam
+{
+    //put your code here
+}//end class
+
+?>
