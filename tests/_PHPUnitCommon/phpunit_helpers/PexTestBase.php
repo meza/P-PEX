86ca6aa9acd5d709b385f02135fc11462e2d5bae
meza <reg@meza.hu>
2010-06-12 15:56:20 +0200

    Added PexTestBase

---
+++ tests/_PHPUnitCommon/phpunit_helpers/PexTestBase.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * PexTestBase.php
+ *
+ * Holds the PexTestBase class
+ *
+ * PHP Version: 5
+ *
+ * @category File
+ * @package
+ * @author   meza <meza@meza.hu>
+ * @license  GPL3.0
+ *                    GNU GENERAL PUBLIC LICENSE
+ *                       Version 3, 29 June 2007
+ *
+ * Copyright (C) 2007 Free Software Foundation, Inc. <http://fsf.org/>
+ * Everyone is permitted to copy and distribute verbatim copies
+ * of this license document, but changing it is not allowed.
+ * @link     http://www.assembla.com/spaces/p-pex
+ */
+
+/**
+ * The PexTestBase class is responsible for tests
+ *
+ * PHP Version: 5
+ *
+ * @category Class
+ * @package
+ * @author   meza <meza@meza.hu>
+ * @license  GPL3.0
+ *                    GNU GENERAL PUBLIC LICENSE
+ *                       Version 3, 29 June 2007
+ *
+ * Copyright (C) 2007 Free Software Foundation, Inc. <http://fsf.org/>
+ * Everyone is permitted to copy and distribute verbatim copies
+ * of this license document, but changing it is not allowed.
+ * @link     http://www.assembla.com/spaces/p-pex
+ */
+class PexTestBase extends MockAmendingTestCaseBase
+{
+    //put your code here
+
+}//end class
+
+?>
